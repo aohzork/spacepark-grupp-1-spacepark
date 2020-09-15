bd451285e4e09ed79657abdfadb59226f6a998dc
Andreas Brochs <55091646+AndreasBrochs@users.noreply.github.com>
2020-09-15 15:49:22 +0200

    parkingSpaceModel

---
+++ SpaceParkAPI/SpaceParkAPI/Models/ParkingSpace.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceParkAPI.Models
+{
+    public class ParkingSpace
+    {
+        [Key]
+        public long ID { get; set; }
+        public int ParkingLotId { get; set; }
+        public int SpaceShipId { get; set; }
+
+
+    }
+}
