eb96eecf31bc91b31662cc3fb9c9a67c81075686
zorkii <micael.wollter@gmail.com>
2020-09-15 15:55:57 +0200

    Update ParkingLot.cs

--- SpaceParkAPI/SpaceParkAPI/Models/ParkingLot.cs
+++ SpaceParkAPI/SpaceParkAPI/Models/ParkingLot.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace SpaceParkAPI.Models
 {
     public class ParkingLot
-    {
+    { 
+        [Key]
+        public long ID { get; set; }
+        public long TotalAmount { get; set; }
     }
 }
