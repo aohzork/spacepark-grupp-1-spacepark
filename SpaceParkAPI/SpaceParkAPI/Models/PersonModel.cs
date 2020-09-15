9037fa25cc81cf9609e5a87e0eba147add62ea9f
simbasang <sebastian.englund@live.se>
2020-09-15 15:29:54 +0200

    added person moedl

---
+++ SpaceParkAPI/SpaceParkAPI/Models/PersonModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceParkAPI.Models
+{
+    public class PersonModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
