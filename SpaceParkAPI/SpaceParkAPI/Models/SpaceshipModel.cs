7ff43aa530cece286e134f8d3a5a773a879ebc2c
Gerglamesh <hampe.kjellstrand@gmail.com>
2020-09-15 15:24:21 +0200

    Add code for spaceship model, won't build yet since there is a relationship to Person, which does'nt exist yet.

--- SpaceParkAPI/SpaceParkAPI/Models/SpaceshipModel.cs
+++ SpaceParkAPI/SpaceParkAPI/Models/SpaceshipModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,8 @@ namespace SpaceParkAPI.Models
 {
     public class SpaceshipModel
     {
+        [Key]
+        public long ID;
+        public PersonModel Person;
     }
 }
