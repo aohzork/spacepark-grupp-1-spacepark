2aad5161ee334dcd9f1e134c66f1cf27efd5a1c6
Gerglamesh <hampe.kjellstrand@gmail.com>
2020-09-16 13:38:25 +0200

    Started work on spaceship repo

---
+++ SpaceParkAPI/SpaceParkAPI/Repos/SpaceshipRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SpaceParkAPI.Models;
+
+namespace SpaceParkAPI.Repos
+{
+    public interface ISpaceshipRepo : IDisposable
+    {
+        SpaceshipModel GetSpaceshipById();
+    }
+}
