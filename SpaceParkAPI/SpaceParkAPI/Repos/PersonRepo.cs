967519b21e7386ccdee2bb824e9eedd7ed3f5129
simbasang <sebastian.englund@live.se>
2020-09-16 13:44:12 +0200

    added

---
+++ SpaceParkAPI/SpaceParkAPI/Repos/PersonRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceParkAPI.Repos
+{
+    public class PersonRepo : IPersonRepo
+    {
+        public string GetPersonName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
