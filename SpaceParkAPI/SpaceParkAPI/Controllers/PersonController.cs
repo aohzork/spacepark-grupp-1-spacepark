a3764af06bd24d3db6228a1f3266839b23773fa5
simbasang <sebastian.englund@live.se>
2020-09-16 14:11:40 +0200

    added first

--- SpaceParkAPI/SpaceParkAPI/Controllers/PersonController.cs
+++ SpaceParkAPI/SpaceParkAPI/Controllers/PersonController.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SpaceParkAPI.Models;
 
 namespace SpaceParkAPI.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class PersonController : Controller
     {
-        
+        [HttpGet("name")]
+        public ActionResult<PersonModel> GetPerson()
+        { 
+        }
     }
 }
