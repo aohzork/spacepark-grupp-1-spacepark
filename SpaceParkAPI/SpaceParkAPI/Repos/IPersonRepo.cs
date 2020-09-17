26acd799f093ac1fc16bded7e4d267712b928530
simbasang <sebastian.englund@live.se>
2020-09-17 15:51:22 +0200

    uppdate

--- SpaceParkAPI/SpaceParkAPI/Repos/IPersonRepo.cs
+++ SpaceParkAPI/SpaceParkAPI/Repos/IPersonRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SpaceParkAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,6 @@ namespace SpaceParkAPI.Repos
 {
     public interface IPersonRepo : IRepository
     {
+        public Task<PersonModel> GetPersonByName(String name);
     }
 }
