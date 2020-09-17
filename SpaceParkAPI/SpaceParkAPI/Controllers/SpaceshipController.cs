2ce9040928c49caf458d17c41a6fe0d6a89d82f4
Gerglamesh <hampe.kjellstrand@gmail.com>
2020-09-17 11:44:30 +0200

    Add controller with Get method for spaceship

---
+++ SpaceParkAPI/SpaceParkAPI/Controllers/SpaceshipController.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using SpaceParkAPI.Models;
+using SpaceParkAPI.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceParkAPI.Controllers
+{
+    public class SpaceshipController : ControllerBase
+    {
+        private readonly ISpaceshipRepo _spaceshipRepo;
+        //private readonly IMapper _mapper;
+
+        public SpaceshipController(ISpaceshipRepo spaceshipRepo /*IMapper mapper*/)
+        {
+            _spaceshipRepo = spaceshipRepo;
+            //_mapper = mapper;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SpaceshipModel>> GetSpaceshipById(int id)
+        {
+            try
+            {
+                var result = await _spaceshipRepo.GetSpaceshipById(id);
+                if (result == null)
+                {
+                    return NotFound($"Spaceship with ID: {id} could not be found");
+                }
+
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Database Failure: {e.Message}");
+            }
+        }
+    }
+}
