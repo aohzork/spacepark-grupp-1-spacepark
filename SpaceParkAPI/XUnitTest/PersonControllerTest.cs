f8b206a2ef45ba39a76c37edb150f7ecc02436bf
simbasang <sebastian.englund@live.se>
2020-09-18 15:55:38 +0200

    first

---
+++ SpaceParkAPI/XUnitTest/PersonControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using SpaceParkAPI.Controllers;
+using SpaceParkAPI.Repos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTest
+{
+    class PersonControllerTest
+    {
+        private readonly PersonRepo _repo;
+        private readonly PersonController _sut;
+        public PersonControllerTest()
+        {
+            _sut = new PersonController(_repo);
+        }
+
+        
+    }
+}
