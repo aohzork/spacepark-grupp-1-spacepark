b77834cdffae1783c818bd6b9da5788f9078b916
simbasang <sebastian.englund@live.se>
2020-09-22 14:30:46 +0200

    get spaceship and person from api

---
+++ SpaceParkFrontend/js/script.js
@@ -0,0 +1,48 @@
+$("#submit").click(GetPersnFromApi)
+
+function GetPersnFromApi()
+{
+    let name=$("#namebox").val();
+
+    let conectionString = `https://swapi.dev/api/people/?search=${name}`;
+    console.log(conectionString)
+    fetch(conectionString)
+    .then(res => res.json())
+    .then(data => {
+        
+        if(data.count==1)
+        {
+            
+          GetShipForPersonFromApi(data.results[0].starships);
+          $("#errormessage").text(`you have successfully parked here ${name}  `)
+
+      }
+      else if(data.count!=0)
+      {
+          $("#errormessage").text(`Pleas typ in your full name `)
+            //här måste man välja vilken person man är 
+      }
+      else
+      {
+          $("#errormessage").text(`no person whit the name ${name} in the database`)
+          console.log("no person whit that name in the database")
+      }
+
+    });
+}
+function GetShipForPersonFromApi(ships)
+{
+
+    let spaceshipNames= [];
+
+    ships.forEach(element => 
+    fetch(element)
+    .then(res => res.json())
+    .then(data => {
+
+        spaceshipNames.push(data.name);
+       
+    })    
+    );
+                
+}
