f49f4a4293fd510668c0fbcc7b75d87fb372614a
Zorkii <56868345+aohzork@users.noreply.github.com>
2020-09-22 22:17:55 +0200

    Update ParkingSpaceControllerTest.cs

--- SpaceParkAPI/XUnitTest/ParkingSpaceControllerTest.cs
+++ SpaceParkAPI/XUnitTest/ParkingSpaceControllerTest.cs
@@ -10,11 +10,42 @@ using Moq.EntityFrameworkCore;
 using SpaceParkAPI.Repos;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using SpaceParkAPI.Controllers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace XUnitTest
 {
-    class ParkingSpaceControllerTest
+    public class ParkingSpaceControllerTest
     {
+        [Fact]
+        public async Task ForSession_ReturnsIdeasForSession()
+        {
+            // Arrange
+            int testParkingSpaceId = 1;
+            var mockRepo = new Mock<IParkingSpaceRepo>();
+            mockRepo.Setup(repo => repo.GetParkingSpaceById(testParkingSpaceId)).Returns(Task.FromResult(GetTestSession()));
+            var controller = new ParkingSpaceController(mockRepo.Object);
+
+            // Act
+            var result = await controller.GetParkingSpaceById(testParkingSpaceId);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = Assert.IsType<ParkingSpaceModel>(okResult.Value);
+            var parkingSpace = returnValue;
+            Assert.Equal(1, parkingSpace.ID);
+        }
+
+        private ParkingSpaceModel GetTestSession()
+        {
+            var session = new ParkingSpaceModel()
+            {                
+                ID = 1
+            };
+
+            return session;
+        }
 
         //[Fact]
         //public async Task ForSession_ReturnsIdeasForSession()
