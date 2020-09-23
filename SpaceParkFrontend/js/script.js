c3497ea651a90df6be4c52af129ed23247235e8d
simbasang <sebastian.englund@live.se>
2020-09-23 15:31:16 +0200

    a

--- SpaceParkFrontend/js/script.js
+++ SpaceParkFrontend/js/script.js
@@ -3,33 +3,34 @@ $("#submit").click(GetPersnFromApi)
 function GetPersnFromApi()
 {
     let name=$("#namebox").val();
-
+    
     let conectionString = `https://swapi.dev/api/people/?search=${name}`;
     console.log(conectionString)
     fetch(conectionString)
-    .then(res => res.json())
-    .then(data => {
+        .then(res => res.json())
+        .then(data => {
         
         if(data.count==1)
         {
             
             GetShipForPersonFromApi(data.results[0].starships);
-          $("#errormessage").text(`you have successfully parked here ${name}  `)
-
-      }
-      else if(data.count!=0)
-      {
-          $("#errormessage").text(`Pleas typ in your full name `)
+            $("#errormessage").text(`you have successfully parked here ${name}  `)
+            
+        }
+        else if(data.count!=0)
+        {
+            $("#errormessage").text(`Pleas typ in your full name `)
             //här måste man välja vilken person man är 
-      }
-      else
-      {
-          $("#errormessage").text(`no person whit the name ${name} in the database`)
-          console.log("no person whit that name in the database")
-      }
-
+        }
+        else
+        {
+            $("#errormessage").text(`no person whit the name ${name} in the database`)
+            console.log("no person whit that name in the database")
+        }
+        
+        alert(conectionString)
     });
-
+    
     function GetShipForPersonFromApi(ships)
 {
 
