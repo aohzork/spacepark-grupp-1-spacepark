23b0be357f6df4791829b90373c9f16d30ada03d
Gerglamesh <hampe.kjellstrand@gmail.com>
2020-09-23 14:08:16 +0200

    Just some work

--- SpaceParkFrontend/js/CheckIfParked.js
+++ SpaceParkFrontend/js/CheckIfParked.js
@@ -1,21 +1,2 @@
-/*(name) => {
-    let parkedPersons = async () => {
-        let response = await fetch(`URL ${name}`);
-        return await response.json();
-    }
-    if (parkedPersons.name)
-};
-
-const IsParked = name => 
-    new Promise((resolves, rejects) => {
-        const api = `https://localhost:44350/api/v1.0/person/${name}`;
-        const request = new XMLHttpRequest();
-        request.open("GET", api, true);
-        request.onload = () => 
-        request.status === 200
-            ? resolves(JSON.parse(request.response).results)
-            : rejects(Error(request.statusText));
-        request.onerror = err => rejects(err);
-        request.send();
-    });
-*/
+let name = "Luke Skywalker";
+fetch(`https://localhost:44350/api/v1.0/person/${name}`).then(res => console.log(res.json())).catch(console.error);
