fc799d7abd876db7ed38e83f321855736347c238
Gerglamesh <hampe.kjellstrand@gmail.com>
2020-09-23 15:03:34 +0200

    Remove faulty line in style.css, refferred object is removed since before

--- SpaceParkFrontend/js/CheckIfParked.js
+++ SpaceParkFrontend/js/CheckIfParked.js
@@ -1,2 +1,4 @@
-let name = "Luke Skywalker";
-fetch(`https://localhost:44350/api/v1.0/person/${name}`).then(res => console.log(res.json())).catch(console.error);
+
+
+let name = "Eric";
+const getParkedPerson = async(name) => fetch(`https://localhost:44350/api/v1.0/person/${name}`).then(res => console.log(res.json())).catch(console.error);
--- SpaceParkFrontend/js/script.js
+++ SpaceParkFrontend/js/script.js
@@ -1,5 +1,2 @@
 
-//IsParked("Eric").then(response => console.log(response.json())).catch(error => console.error(`Request failed: ${error}`));
-//console.log(IsParked("Eric"));
-let name = "Luke Skywalker";
-fetch(`https://localhost:44350/api/v1.0/person/${name}`).then(res => console.log(res.json())).catch(console.error);
+
