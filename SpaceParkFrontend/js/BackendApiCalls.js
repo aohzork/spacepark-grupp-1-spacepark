3520cdb177bbbc058dcbb69ec6342576b82e6c1d
Gerglamesh <hampe.kjellstrand@gmail.com>
2020-09-24 10:41:39 +0200

    Change name of method GetParkedPerson to getPerson

--- SpaceParkFrontend/js/BackendApiCalls.js
+++ SpaceParkFrontend/js/BackendApiCalls.js
@@ -1,7 +1,8 @@
 //Method making a call to our api to fetch a person by name.
-const getParkedPerson = async(name) => 
+const getPerson = async(name) => 
     fetch(`https://localhost:44350/api/v1.0/person/${name}`)
     .then( response => {
         return response.json();
     })
     .catch(console.error);
+    
