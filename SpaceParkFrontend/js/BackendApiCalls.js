f92e5c0494cec5c0354f848ae6af9270f87fc1f7
simbasang <sebastian.englund@live.se>
2020-09-29 13:57:01 +0200

    gg

--- SpaceParkFrontend/js/BackendApiCalls.js
+++ SpaceParkFrontend/js/BackendApiCalls.js
@@ -60,6 +60,55 @@ const getParkedPerson = async(name) =>
     .catch(console.error);
 
 
+    const getPerson = async(name) => {
+        try {
+            let response = await fetch(`https://localhost:44350/api/v1.0/person/${name}`, 
+            {method: 'GET'});
+            let json = response.json();
+            return json;
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    
+    //Method making a call to our api to post a person by name.
+    const postPerson = async(personObject) => {
+        try {
+            
+            //Do request
+            let response = await fetch(`https://localhost:44350/api/v1.0/person`, 
+            {
+                method: 'POST',
+                headers: {'Content-Type': `application/json`},
+                body: personObject.ToJsonString()
+            });
+            
+            //Log the response to console
+            console.log(response);
+            
+            //Get the response body as json and return it
+            let json = response.json();
+            return json;
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    
+    //Method making a call to our api to delete a person by name.
+    const deletePerson = async(name) => {
+        try {
+            let response = await fetch(`https://localhost:44350/api/v1.0/person/${name}`, 
+            {method: 'DELETE'});
+            return response.json;
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    /****************************************************
+------------------- PARKINGSPACE CALLS --------------------
+****************************************************/
+
+//Method making a call to our api to fetch a parkingspace by id.
     const GetParkingspaceByID = async(id) => 
     fetch(`https://localhost:44350/api/v1.0/ParkingSpace/${id}`)
     .then( response => {
@@ -67,47 +116,3 @@ const getParkedPerson = async(name) =>
         return response.json();
     })
     .catch(console.error);
-const getPerson = async(name) => {
-    try {
-        let response = await fetch(`https://localhost:44350/api/v1.0/person/${name}`, 
-            {method: 'GET'});
-        let json = response.json();
-        return json;
-    } catch (error) {
-        console.error(error);
-    }
-};
-
-//Method making a call to our api to post a person by name.
-const postPerson = async(personObject) => {
-    try {
-
-        //Do request
-        let response = await fetch(`https://localhost:44350/api/v1.0/person`, 
-            {
-                method: 'POST',
-                headers: {'Content-Type': `application/json`},
-                body: personObject.ToJsonString()
-            });
-
-        //Log the response to console
-        console.log(response);
-
-        //Get the response body as json and return it
-        let json = response.json();
-        return json;
-    } catch (error) {
-        console.error(error);
-    }
-};
-    
-//Method making a call to our api to delete a person by name.
-const deletePerson = async(name) => {
-    try {
-        let response = await fetch(`https://localhost:44350/api/v1.0/person/${name}`, 
-            {method: 'DELETE'});
-        return response.json;
-    } catch (error) {
-        console.error(error);
-    }
-};
