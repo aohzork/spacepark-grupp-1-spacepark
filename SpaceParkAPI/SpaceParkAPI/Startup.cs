ec454280787c03bd4a12912e92796329bbf287c3
Gerglamesh <hampe.kjellstrand@gmail.com>
2020-09-30 09:35:50 +0200

    Add connection to Frontend in CORS to allow communication across origins

--- SpaceParkAPI/SpaceParkAPI/Startup.cs
+++ SpaceParkAPI/SpaceParkAPI/Startup.cs
@@ -33,7 +33,8 @@ namespace SpaceParkAPI
                     "AllowFrontEnd",
                     builder =>
                     {
-                        builder.WithOrigins("http://127.0.0.1:5500")
+                        builder.WithOrigins("http://127.0.0.1:5500",
+                                            "https://frontendwebsw.azurewebsites.net/")
                         .AllowAnyMethod()
                         .AllowAnyHeader();
                     });
